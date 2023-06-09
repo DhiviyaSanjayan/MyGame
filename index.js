7eeabe06ea41d00d69f2201e78d99557726eab78
Dhiviya <122795939+DhiviyaSanjayan@users.noreply.github.com>
2023-06-09 09:52:07 +0100

    start of while function

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const randomNum = Math.floor(Math.random() * 3) + 1;
+// 3 options -"rock", "paper" and "scissors"
+
+let options = ["rock", "paper", "scissors"];
+let randomNum = Math.floor(Math.random() * options.length);
+return options[randomNum];
+
+let playerScore = 0:
+let computerScore = 0;
+
+function playGame(){
+    // while??
+    while (playerScore < 3 && computerScore < 3) {
+        let playerChoice = "rock";
+        let computerChoice = 
+    }
+}
